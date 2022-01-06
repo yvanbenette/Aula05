946c0d3047e50aeac600a13ff168f7c689c12ade
Yvan Benette <yvan.benette@gmail.com>
2022-01-06 16:39:22 -0300

    menu agora apresenta a opção 4

--- src/TeacherHelper.java
+++ src/TeacherHelper.java
@@ -18,6 +18,8 @@ public class TeacherHelper {
 			System.out.println("1 - Calcular salário");
 			System.out.println("2 - Calcular média de notas dos alunos");
 			System.out.println("3 - Exibir a motivação do dia!");
+			System.out.println("4 - Sair do sistema");
+		
 			opcao = leitor.nextInt();
 			
 			switch (opcao) {
